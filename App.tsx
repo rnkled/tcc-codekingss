5be114e7ebfeab7b2e930e5340035fd9977a5306
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-08-18 14:21:44 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,8 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Routes from "./src/routes/index";
+import { useFonts, Inter_600SemiBold, Inter_300Light, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import Loading from './src/components/Loading';
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_600SemiBold,
+    Inter_300Light,
+    Inter_400Regular,
+    Inter_500Medium
+
+  });
+
+
+   if (!fontsLoaded) {
+    return <Loading/>;
+  }
+
   return (
     <View style={{ flex: 1, width: '100%'}}>
         <Routes/>
