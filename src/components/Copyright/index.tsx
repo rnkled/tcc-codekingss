36f9849794ca2dba9cda5bee2b5c69685fd2c803
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-08-19 08:47:05 -0300

    Create index.tsx

---
+++ src/components/Copyright/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Copyright: React.FC = () => {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.label}>Powered By CodeKinkss</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 35,
+  },
+
+  label: {
+    color: "#8B97FF",
+    fontSize: 16,
+    fontFamily: "Inter_400Regular"
+  }
+})
+
+export default Copyright;
