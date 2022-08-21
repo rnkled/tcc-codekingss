34bad680b1ccbbbe388201ebe4a51862bc2e4600
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-08-21 20:22:30 -0300

    Update index.tsx

--- src/pages/VideoCall/index.tsx
+++ src/pages/VideoCall/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
-import { Dimensions, StyleSheet, Text } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text } from 'react-native';
 import { View } from 'react-native';
 import Header from '../../components/Header';
 import AgoraUIKit, {VideoRenderMode} from 'agora-rn-uikit';
 import { useNavigation } from '@react-navigation/native';
-
+import Background from '../../components/Background';
+import { Ionicons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 
 const VideoCall: React.FC = () => {
@@ -35,7 +37,7 @@ const VideoCall: React.FC = () => {
           muteLocalAudio: {backgroundColor: "#8B97FF", borderWidth: 0 },
           muteLocalVideo: {backgroundColor: "#8B97FF", borderWidth: 0 },
           switchCamera: {backgroundColor: "#8B97FF", borderWidth: 0 },
-          endCall: {backgroundColor: '#f66', borderWidth: 0},
+          endCall: {backgroundColor: '#f66', borderWidth: 0, },
 
         },
         localBtnContainer: {
@@ -74,6 +76,7 @@ const VideoCall: React.FC = () => {
         initialDualStreamMode: 1,
         dual: true,
         
+        
 
       }}
       connectionData={connectionData} 
@@ -82,19 +85,92 @@ const VideoCall: React.FC = () => {
     
     />
   ) : (
-    <Text onPress={()=>setVideoCall(true)}>Start Call</Text>
+    <Background style={styles.container}> 
+      <Text style={styles.titlePage}>Encontrando um profissional...</Text>
+      <View style={styles.contentPrimary}>
+        <View style={styles.circleContent}>
+          <Ionicons name="videocam-outline" size={90} color="#8B97FF" />
+        </View>
+        <View style={styles.messageContent}>
+          <Text style={styles.message}>Para sua melhor comodidade, ative o vídeo da chamada somente se sentir-se confortável.</Text>
+        </View>
+        <View style={styles.buttonsContent}>
+          <TouchableOpacity activeOpacity={0.8} style={styles.buttonRadio}>
+            <Feather name="mic-off" size={30} color="#0C0150" />
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.8} style={styles.buttonRadio}>
+            <Feather name="video-off" size={30} color="#0C0150" />
+          </TouchableOpacity>
+
+        </View>
+
+      </View>
+    </Background>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0C0150"
+    alignItems: "center",
+    paddingTop: 50
   },
 
   contentPrimary: {
+    width: "100%",
     flex: 1,
-    justifyContent: "center"
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: 90,
+  },
+
+  titlePage: {
+    color: "#8B97FF",
+    fontSize: 21,
+    fontFamily: "Inter_600SemiBold"
+  },
+
+  circleContent: {
+    width: 180,
+    height: 180,
+    borderRadius: 180 / 2,
+    backgroundColor: "transparent",
+    borderWidth: 1.9,
+    borderColor: "#8B97FF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  messageContent: {
+    width: "100%",
+    height: "auto",
+    paddingHorizontal: 10
+  },
+  message: {
+    textAlign: "center",
+    fontSize: 16,
+    color: "#8B97FF",
+    fontFamily: "Inter_400Regular"
+  },
+
+  buttonsContent: {
+    width: "100%",
+    height: "20%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignContent: 'center'
+  },
+
+  buttonRadio: {
+    width: 70,
+    height: 70,
+    borderRadius: 70/2,
+    backgroundColor: "#8B97FF",
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 5
+  
+
   }
 })
 
