c716c9ae06c9658ab900cab2c5a8c7ec2f4178f6
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-08-21 17:37:12 -0300

    Update index.tsx

--- src/pages/VideoCall/index.tsx
+++ src/pages/VideoCall/index.tsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { Dimensions, StyleSheet, Text } from 'react-native';
 import { View } from 'react-native';
 import Header from '../../components/Header';
-import AgoraUIKit from 'agora-rn-uikit';
+import AgoraUIKit, {VideoRenderMode} from 'agora-rn-uikit';
 import { useNavigation } from '@react-navigation/native';
-import {VideoRenderMode} from 'react-native-agora';
+
 
 
 const VideoCall: React.FC = () => {
@@ -14,40 +14,72 @@ const VideoCall: React.FC = () => {
   const connectionData = {
     appId: '885ca3cade8f4a3e81c7550a827300a2',
     channel: 'test',
+    
   };
   
   const rtcCallbacks = {
     EndCall: () => setVideoCall(false),
+  
+   
+    
     
   };
   
 
   return videoCall ? (
     <AgoraUIKit styleProps={{
-        remoteBtnStyles: {
-          muteRemoteAudio: { display: "none", },
-          muteRemoteVideo: { display: "none" },
+        // iconSize: 30,
+        // theme: "#0C0150",
+        // borderColor: "#8B97FF"
+        localBtnStyles:{
+          muteLocalAudio: {backgroundColor: "#8B97FF", borderWidth: 0 },
+          muteLocalVideo: {backgroundColor: "#8B97FF", borderWidth: 0 },
+          switchCamera: {backgroundColor: "#8B97FF", borderWidth: 0 },
+          endCall: {backgroundColor: '#f66', borderWidth: 0},
 
-          
         },
+        localBtnContainer: {
+          backgroundColor: '#0C0150',
+          bottom: 0,
+          paddingVertical: 10,
+          height: 80,
+        },
+        
+        UIKitContainer: {height: '94%'},
+      //   minViewContainer: {
+      //   bottom: 80,
+      //   top: undefined,
+      //   backgroundColor: '#fff',
+      //   borderWidth: 4,
+      //   height: '100%',
+      // },
+  
+        iconSize: 30,
+        videoMode: {
+          max: VideoRenderMode.Hidden,
+          min: VideoRenderMode.Hidden,
+        },
+        
+        // videoPlaceholderContainer: {backgroundColor: 'red'}
+
+
+        
     
-        minViewStyles: { height: 200, width: 150 },
-        overlayContainer: {
-          width: 150,
-          height: 50,
-          paddingRight: 90,
-        },
+        
       }}   
       settings={{
-        mode: 1,
-        role: 1,
+        mode: 2,
+        role: 1, // analisar o acesso do psicologo aqui
         activeSpeaker: false,
         initialDualStreamMode: 1,
-        dual: true
+        dual: true,
+        
 
       }}
       connectionData={connectionData} 
-      rtcCallbacks={rtcCallbacks} 
+      rtcCallbacks={rtcCallbacks}
+  
+    
     />
   ) : (
     <Text onPress={()=>setVideoCall(true)}>Start Call</Text>
