1aae8a7b74fd52818bcf5aa71b3ffd917e4ecae3
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-08-24 13:44:58 -0300

    Create index.tsx

---
+++ src/components/CardComment/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const CardComment: React.FC = () => {
+  return(
+    <View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    height: "auto",
+  }
+})
+
+export default CardComment;
