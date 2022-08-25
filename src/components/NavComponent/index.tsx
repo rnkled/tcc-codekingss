f0269b23d3c67cacc1ac1efc7798084b259f1e44
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-08-25 09:19:08 -0300

    Update index.tsx

--- src/components/NavComponent/index.tsx
+++ src/components/NavComponent/index.tsx
@@ -1,62 +1,88 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Tab, Text as TextTab, TabView } from '@rneui/themed';
+import CardComment from '../CardComment';
 
 // import { Container } from './styles';
 
 type Props = {
-  buttonLeftPressed: boolean,
-  buttonRightPressed: boolean,
-  onPressButtonLeft: () => void, 
-  onPressButtonRight: () => void,
+  data?: {
+    informationPersonal: {
 
+    },
+    evaluations: []
+  }
 }
 
 
-const NavComponent = ({buttonLeftPressed, buttonRightPressed, onPressButtonLeft, onPressButtonRight} : Props) => {
+const NavComponent = ({data} : Props) => {
+  const [index, setIndex] = useState(0);
   return(
-    <View style={styles.container}>
-      <TouchableOpacity activeOpacity={0.8} onPress={onPressButtonLeft} style={buttonLeftPressed ? styles.buttonNavPressed : styles.buttonNav}>
-        <Text style={[styles.labelNavButton, {color: buttonLeftPressed ? "#8B97FF" : "#828282"}]}>Avaliações</Text>
-      </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.8} onPress={onPressButtonRight} style={buttonRightPressed ? styles.buttonNavPressed : styles.buttonNav}>
-        <Text style={[styles.labelNavButton, {color: buttonRightPressed ? "#8B97FF" : "#828282"}]}>Informações</Text>
-      </TouchableOpacity>
-    </View>
+    <>
+    <Tab 
+      value={index}
+      onChange={(e) => setIndex(e)}
+      variant="primary"
+      containerStyle={styles.container}
+      indicatorStyle={{display: "none"}}
+    >
+      <Tab.Item
+        title="Avaliações"
+        size='sm'
+        titleStyle={{ fontSize: 18, color: `${index === 0 ? '#8B97FF' : '#828282'}`, fontFamily: "Inter_400Regular"  }}
+        containerStyle={[styles.buttonNav, {backgroundColor: `${index === 0 ? "#0C0150" : "#EEE"}`}]}
+        
+        /> 
+      <Tab.Item
+        title="Informações"
+        size='sm'
+        titleStyle={{ fontSize: 18, color: `${index === 1 ? '#8B97FF' : '#828282'}`, fontFamily: "Inter_400Regular" }}
+        containerStyle={[styles.buttonNav, {backgroundColor: `${index === 1 ? "#0C0150" : "#EEE"}`}]}   
+        />
+      </Tab>
+
+      <TabView value={index} onChange={setIndex} animationType="spring">
+        <TabView.Item style={styles.contentDescription}>
+          <>
+            <CardComment />
+            <CardComment />
+          </>
+          
+        </TabView.Item>
+        <TabView.Item style={styles.contentDescription}>
+          
+        </TabView.Item>
+    </TabView>
+     </>
+      
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: 50,
-    flexDirection: "row",
     backgroundColor: "#EEEEEE",
+    height: 50,
     borderRadius: 100,
+  
   },
 
   buttonNav: {
-    width: "50%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 100,
-  },
-
-  buttonNavPressed: {
-    backgroundColor: "#0C0150",
+    backgroundColor: "#EEEE",
     borderRadius: 50,
-    width: "50%",
+    borderWidth: 1,
+    borderColor: "#EEE",
     justifyContent: "center",
     alignItems: "center",
-    height: "100%",
-    borderColor: "#EEE",
-    borderWidth: 0.9
+    
   },
 
-  labelNavButton: {
-    fontSize: 18,
-    fontFamily: "Inter_400Regular",
+  contentDescription: {
+    flex: 1,
+    width: "100%",
+    padding: 10,
   }
+
+
 })
 
 export default NavComponent;
