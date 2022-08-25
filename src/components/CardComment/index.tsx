e77a0498b58e7066001f83dc8b3da0b5f6edffab
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-08-25 09:19:06 -0300

    Update index.tsx

--- src/components/CardComment/index.tsx
+++ src/components/CardComment/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
+import { Image, ImageSourcePropType, StyleSheet, Text, View } from 'react-native';
+import imagePacient from "../../assets/th.jpg";
 // import { Container } from './styles';
 
-const CardComment: React.FC = () => {
-  return(
-    <View>
+// type Props = {
+//   dataComment: {
+//     pacientName: string;
+//     sourceImg: ImageSourcePropType;
+//     description: string;
+//     timesTamp: string;
+//   }
+// }
 
+const CardComment = ({ dataComment }: Props) => {
+  return(
+    <View style={styles.container}>
+      <View style={styles.contentImage}>
+        <Image style={styles.imageStyle} source={imagePacient} />
+      </View>
+      <View style={styles.contentDescription}>
+        <View style={styles.contentInformationComment}>
+          <Text style={styles.labelName}>Maria</Text>
+          <Text style={styles.labelTimer}>8m atrás</Text>
+        </View>
+        <Text style={styles.labelComment}>Amei sua consulta!</Text>
+      </View>
     </View>
   );
 }
@@ -15,6 +33,53 @@ const styles = StyleSheet.create({
   container: {
     width: "100%",
     height: "auto",
+    flexDirection: "row",
+    paddingTop: 10,
+    alignItems: "center"
+  },
+
+  contentImage: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 10,
+    marginRight: 15,
+  },
+
+  labelName: {
+    fontSize: 20,
+    color: "#FFF",
+    fontFamily: "Inter_500Medium"
+  },
+  
+  labelComment: {
+    fontSize: 16,
+    color: "#FFF",
+    lineHeight: 30,
+    fontFamily: "Inter_400Regular"
+  },
+
+  labelTimer: {
+    fontSize: 16,
+    fontFamily: "Inter_400Regular",
+    color: "#828282",
+  },
+
+  contentDescription: {
+    width: "75%",
+    maxWidth: "75%",
+    height: "auto",
+  },
+
+  contentInformationComment: {
+    justifyContent: "space-between",
+    flexDirection: "row"
+  },
+
+  imageStyle: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
   }
 })
 
