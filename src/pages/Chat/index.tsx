f99487e978f9e1050a2b5c6f263f9789039a3e78
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-08-29 21:32:14 -0300

    Update index.tsx

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,55 +1,78 @@
-import { useNavigation } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack/lib/typescript/src/types';
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
+import { AutoScrollFlatList } from 'react-native-autoscroll-flatlist';
 import Background from '../../components/Background';
 import ChatBoxMessage, { ChatBoxProps } from '../../components/ChatBoxMessage';
 import Header from '../../components/Header';
 import InputTextChat from '../../components/InputTextChat';
 import { RouteStackParamList } from '../../routes';
+import database from '@react-native-firebase/database';
+import AuthContext from '../../context/AuthContext';
 
 // import { Container } from './styles';
 type propsScreens = NativeStackNavigationProp<RouteStackParamList>
+
+type paramsRoute = {
+  id_professional: string;
+}
 
 const Chat: React.FC = () => {
   const navigation = useNavigation<propsScreens>();
 
   const [message, setMessage] = useState("");
-  const [messages, setMessages] = useState<ChatBoxProps[]>([
-    {message: "Oooooooooiii", user: "pacient"},
-    {message: "tudooooo beeeeem?", user: "pacient"},
-    {message: "Olá, boa tarde, tudo sim e você?", user: "professional"},
-    {message: "Euuu estoooou beeeem", user: "pacient"},
-    // {message: "Oooooooooiii", user: "pacient"},
-    // {message: "Oooooooooiii", user: "pacient"},
-    // {message: "Oooooooooiii", user: "pacient"},
-
-    // {message: "Oooooooooiii", user: "pacient"},
-    // {message: "Oooooooooiii", user: "pacient"},
-    // {message: "Oooooooooiii", user: "pacient"},
-    // {message: "Oooooooooiii", user: "pacient"},
-    // {message: "Oooooooooiii", user: "pacient"},
-
-
-
-  ])
+  const [messages, setMessages] = useState<ChatBoxProps[]>([]);
+  const route = useRoute<RouteProp<RouteStackParamList, "chat">>();
+  const {user} = useContext(AuthContext);
+  
+  useEffect(() => {
+    getMessage();
+    
+  }, [])
 
   function goBack(){
     navigation.goBack();
   }
 
+  function getMessage(){
+    database().ref(`/chats/${user._id}/${route.params.id_professional}/messages/`).orderByChild('id').on("value", snapshot => {
+        var messagesArr = [];
+        snapshot && snapshot.forEach((childSnapshot) => {
+          let objMessage = childSnapshot.val();
+          messagesArr.push(objMessage);
+          if(childSnapshot.numChildren() <= 0){      
+            setMessages([]);
+            return true;
+          }
+        });
+
+        setMessages(messagesArr) 
+      
+
+      //   const dataMessages = [];
+      //   dataMessages.push(snapshot.val());
+      //   let objectMessage = Object.keys(dataMessages[0]).map(key => dataMessages[0][key]);
+      //   console.log({objectMessage});
+        
+        
+      // }else{
+      //   setMessages([])
+  })
+  
+  }
+
   function handleChatMessage(){
     if(message){
-      let arrayMessage = [...messages];
-      let messageObject: ChatBoxProps;
-      messageObject = {
+      const newReferenceMessage = database().ref(`/chats/${user._id}/${route.params.id_professional}/messages`).push();
+      const messageObject = {
+        id_user: 4,
+        id: Date.now(),
         message,
-        user: "pacient",
+        sent: new Date(Date.now()),
+        user_type: "pacient"
       }
-      arrayMessage.push(messageObject);
-      setMessages(arrayMessage);
-      setMessage("");
-    
+      newReferenceMessage.set(messageObject).then(() => setMessage(""));
     }
   }
 
@@ -57,12 +80,11 @@ const Chat: React.FC = () => {
     <Background style={styles.container}>
         <Header buttonLeft={{label: "Voltar", onPress: goBack,}} buttonRight={{label: "Opções", onPress: goBack}} titlePage="Victoria" fontSize={24}/>
         <View style={styles.contentChatMessages}>
-        <FlatList
+        <AutoScrollFlatList
           data={messages}
-          style={{width: "100%"}}
-          contentContainerStyle={{height: "100%", justifyContent: 'flex-end' }}
+          contentContainerStyle={{width: "100%",}}
           renderItem={({item}) => (
-            <View style={{alignItems: item.user === "pacient" ? "flex-end" : "flex-start",}}>
+            <View style={{alignItems: item.user_type === "pacient" ? "flex-end" : "flex-start",}}>
               <ChatBoxMessage data={item} />
             </View>  
           )}
@@ -87,7 +109,6 @@ const styles = StyleSheet.create({
     width: "100%",
     flex: 1,
     padding: 15,
-    alignItems: "flex-end",
 
    
   },
