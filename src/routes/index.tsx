325aca3518b857038dc94942fa9659742bb6db9c
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-09-02 17:19:55 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,8 @@ import RateCallVideo from '../pages/RateCallVideo';
 import AuthContext from "../context/AuthContext";
 import ProfessionalProfile from '../pages/ProfessionalProfile';
 import Chat from '../pages/Chat';
+import HomeProfessional from '../pages/HomeProfessional';
+import SearchProfessional from '../pages/SearchProfessional';
 
 export type RouteStackParamList = {
     login: undefined,
@@ -15,6 +17,8 @@ export type RouteStackParamList = {
     'home.index': undefined;
     'home.search': undefined;
     'home.settings': undefined;
+    'home.searchpacients': undefined;
+    'home.indexprofessional': undefined;
     videoCall: undefined;
     rateVideoCall: {
         id_professional: string,
@@ -24,6 +28,8 @@ export type RouteStackParamList = {
     },
     chat: {
         id_professional: string;
+        id_pacient: string;
+        pushNotification: string;
     },
 }
 
@@ -31,7 +37,7 @@ const Stack = createNativeStackNavigator<RouteStackParamList>();
 
 const Routes = () => {
 
-    const {signed } =  useContext(AuthContext);
+    const {signed, user } =  useContext(AuthContext);
 
     return (
             <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -39,7 +45,8 @@ const Routes = () => {
                     <>
                         <Stack.Screen name="login" component={LoginStack}/>
                     </>
-                    ) : (
+                    ) : user.role === "user" ? (
+                        
                     <>
                         <Stack.Screen name="home" component={HomeStack} />
                         <Stack.Screen name="videoCall" component={VideoCall} />
@@ -49,6 +56,16 @@ const Routes = () => {
 
 
                     </>
+                ) : (
+                     <>
+                        <Stack.Screen name="home.indexprofessional" component={HomeProfessional} />       
+                        <Stack.Screen name="home.searchpacients" component={SearchProfessional} />
+                        <Stack.Screen name="chat" component={Chat} />
+
+
+
+                    </>
+
                 )}
             </Stack.Navigator>)
 }
