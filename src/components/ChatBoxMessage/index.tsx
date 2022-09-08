7eb511fdb9fcb39ea6b6a283ca28a361311f463e
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-09-08 13:40:07 -0300

    Update index.tsx

--- src/components/ChatBoxMessage/index.tsx
+++ src/components/ChatBoxMessage/index.tsx
@@ -1,11 +1,13 @@
-import React, { useContext } from 'react';
+import React, { memo, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AuthContext from '../../context/AuthContext';
+import moment from "moment";
 
 export type ChatBoxProps = {
   user_type: "pacient" | "professional",
   message: string;
   id_user: string;
+  sent: string;
 }
 
 type Props = {
@@ -21,6 +23,10 @@ const ChatBoxMessage = ({data, user_role}: Props) => {
       <Text style={styles.textMessage}>
         {data.message}
       </Text>
+      <Text style={styles.labelData}>
+        {/* {moment(String(data.sent).split("-")[1], "HH:mm:ss" ).format("HH:mm")} */}
+        {String(data.sent).split("-")[1]}
+      </Text>
 
     </View>
   );
@@ -30,7 +36,9 @@ const styles = StyleSheet.create({
   container: {
     width: "auto",
     maxWidth: 235,
-    padding: 10,
+    paddingHorizontal: 10,
+    paddingTop: 10,
+    paddingBottom: 5, 
     marginBottom: 10,
     borderTopEndRadius: 15,
     borderTopLeftRadius: 15,
@@ -41,7 +49,17 @@ const styles = StyleSheet.create({
     fontFamily: "Inter_400Regular",
     color: "#0C0150",
     fontSize: 16,
+    marginRight: 55,
+  },
+
+  labelData: {
+    fontFamily: "Inter_400Regular",
+    fontSize: 12,
+    textAlign: "right",
+    bottom: 3,
+
+
   }
 })
 
-export default ChatBoxMessage;
+export default memo(ChatBoxMessage);
