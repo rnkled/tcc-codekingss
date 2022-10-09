9cc5e357fa7460f96d6cb47429fda4a98d51c0da
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-10-09 17:38:00 -0300

    Update MainActivity.java

--- android/app/src/main/java/com/tcccodekingss/MainActivity.java
+++ android/app/src/main/java/com/tcccodekingss/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends ReactActivity {
               .setUsage(AudioAttributes.USAGE_NOTIFICATION)
               .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
               .build();
-      notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/mixkit"), att);
+      notificationChannel.setSound(Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/elegant"), att);
       notificationChannel.enableVibration(true);
       notificationChannel.setVibrationPattern(new long[]{400, 400});
       notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_PUBLIC);
