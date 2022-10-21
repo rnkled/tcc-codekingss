0cbfcca0705a60590de3f2d254fbf18ecb934bf0
Kener Wendel <kenerwendel@gmail.com>
2022-10-21 14:28:25 -0300

    Added Heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "https://management-microservice-production.up.railway.app",
+  baseURL: "https://servidor-painel-ettos.herokuapp.com/",
   // baseURL: "http://192.168.0.108:5555",
-  baseURL: "http://192.168.100.25:5555",
+  // baseURL: "http://192.168.100.25:5555",
   headers: {
     "Content-type": "application/json"
   }
