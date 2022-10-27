93f7aadb9328021548ed9b14c55bee815b4c6fc7
Pedro Henrique <64162307+PedroHenrike20@users.noreply.github.com>
2022-10-27 14:01:56 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,43 +1,36 @@
-import React, {useContext, useEffect, useState} from 'react';
-import { StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native';
-import Header from '../../components/Header';
-import { EvilIcons, Ionicons } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import Footer from '../../components/Footer';
-import Button from '../../components/Button';
-import { useNavigation } from '@react-navigation/native';
-import Background from '../../components/Background';
+import React, { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Alert } from "react-native";
+import Header from "../../components/Header";
+import { EvilIcons, Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import Footer from "../../components/Footer";
+import Button from "../../components/Button";
+import { useNavigation } from "@react-navigation/native";
+import Background from "../../components/Background";
 import AuthContext from "../../context/AuthContext";
 import CircleAnimation from "../../assets/effectButton.json";
-import { RouteStackParamList } from '../../routes';
-import { Notifier, Easing } from 'react-native-notifier';
-import messaging from '@react-native-firebase/messaging';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { LinearGradient } from 'expo-linear-gradient';
-import AnimatedLottieView from 'lottie-react-native';
-import ThemeContext from '../../context/ThemeContext';
-import { Theme } from '../../interfaces/themeInterface';
+import { RouteStackParamList } from "../../routes";
+import { Notifier, Easing } from "react-native-notifier";
+import messaging from "@react-native-firebase/messaging";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { LinearGradient } from "expo-linear-gradient";
+import AnimatedLottieView from "lottie-react-native";
+import ThemeContext from "../../context/ThemeContext";
+import { Theme } from "../../interfaces/themeInterface";
 
-type propsScreens = NativeStackNavigationProp<RouteStackParamList>
-
-
+type propsScreens = NativeStackNavigationProp<RouteStackParamList>;
 
 const Home: React.FC = () => {
-  const {theme} = useContext(ThemeContext);
-  const styles = React.useMemo(
-    () => createStyles(theme),
-    [theme]
-  );
-  
+  const { theme } = useContext(ThemeContext);
+  const styles = React.useMemo(() => createStyles(theme), [theme]);
+
   const navigation = useNavigation<propsScreens>();
 
-  const {signOut, user} =  useContext(AuthContext);
+  const { signOut, user } = useContext(AuthContext);
 
-   useEffect(() => {
-    console.log(user);
-    
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      if(remoteMessage.data.type && remoteMessage.data.type === "chat"){
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      if (remoteMessage.data.type && remoteMessage.data.type === "chat") {
         Notifier.showNotification({
           title: `${remoteMessage.notification.title}`,
           description: `${remoteMessage.notification.body}`,
@@ -45,26 +38,29 @@ const Home: React.FC = () => {
           translucentStatusBar: true,
           showAnimationDuration: 800,
           showEasing: Easing.bounce,
-          onHidden: () => console.log('Hidden'),
+          onHidden: () => console.log("Hidden"),
           onPress: () => {
-            if(user.role === "user"){
+            if (user.role === "user") {
               navigation.navigate("chat", {
                 id_professional: remoteMessage.data.id_professional,
                 pushNotification: user.tokenPush,
                 id_pacient: undefined,
-                name: remoteMessage.data.name
-              })
+                name: remoteMessage.data.name,
+              });
             }
-            if(user.role === "admin"){
-              
+            if (user.role === "admin") {
             }
           },
           hideOnPress: false,
           componentProps: {
-            titleStyle: {color: theme.secondary, fontSize: 18, fontFamily: "Inter_500Medium"},
-            descriptionStyle: {fontFamily: "Inter_400Regular"},
-            containerStyle: {backgroundColor: theme.backgroundVariant}
-          }
+            titleStyle: {
+              color: theme.secondary,
+              fontSize: 18,
+              fontFamily: "Inter_500Medium",
+            },
+            descriptionStyle: { fontFamily: "Inter_400Regular" },
+            containerStyle: { backgroundColor: theme.backgroundVariant },
+          },
         });
       }
     });
@@ -72,30 +68,23 @@ const Home: React.FC = () => {
     return unsubscribe;
   }, []);
 
-
-  function handleNewVideoCall(){
-
-    navigation.navigate("videoCall", {channel_id: null});
-
+  function handleNewVideoCall() {
+    navigation.navigate("videoCall", { channel_id: null });
   }
 
   function handleLogOut() {
-    Alert.alert(
-      "Sair",
-      "Tem certeza que deseja sair de sua conta?",
-      [
-        {
-          text: "Cancelar",
-          onPress: () => {}
+    Alert.alert("Sair", "Tem certeza que deseja sair de sua conta?", [
+      {
+        text: "Cancelar",
+        onPress: () => {},
+      },
+      {
+        text: "Sim",
+        onPress: () => {
+          signOut();
         },
-        {
-          text: "Sim",
-          onPress: () => {
-            signOut();
-          }
-        }
-      ],
-    );
+      },
+    ]);
   }
 
   function goToSearch() {
@@ -107,61 +96,74 @@ const Home: React.FC = () => {
   }
 
   function goToCalendar() {
-    console.log('teste');
-    
+    console.log("teste");
+
     navigation.navigate("calendar");
   }
 
-
-  return(
+  return (
     <Background>
-      <Header 
-        titlePage={"Bem-Vindo " + (user ? user.name.split(" ")[0] : '')}
+      <Header
+        titlePage={"Bem-Vindo " + user?.name.split(" ")[0]}
         fontSize={18}
         buttonLeft={{
           isIcon: true,
-          icon: () => <Ionicons name="settings-outline"  size={30} color={theme.primaryVariant}/>,
+          icon: () => (
+            <Ionicons
+              name="settings-outline"
+              size={30}
+              color={theme.primaryVariant}
+            />
+          ),
           onPress: goToSettings,
-        }} 
+        }}
         buttonRight={{
           isIcon: true,
-          icon: () =><MaterialIcons name="logout" size={30} color={theme.primaryVariant} />,
+          icon: () => (
+            <MaterialIcons
+              name="logout"
+              size={30}
+              color={theme.primaryVariant}
+            />
+          ),
           onPress: handleLogOut,
         }}
       />
       <View style={styles.contentPrimary}>
-       
-        <TouchableOpacity onPress={handleNewVideoCall} activeOpacity={0.8} style={styles.buttonCircle}>
+        <TouchableOpacity
+          onPress={handleNewVideoCall}
+          activeOpacity={0.8}
+          style={styles.buttonCircle}
+        >
           <LinearGradient
             colors={[theme.buttonCircleFirst, theme.buttonCircleSecond]}
             style={styles.contentBorderButton}
           >
-                <Text style={styles.labelButtonCircle}>Atendimento Rápido</Text>
+            <Text style={styles.labelButtonCircle}>Atendimento Rápido</Text>
           </LinearGradient>
         </TouchableOpacity>
-
       </View>
       <View style={styles.contentSecondary}>
-        <Button label='Minhas consultas' onPress={goToCalendar} />
-        <Button label='Buscar profissionais' onPress={goToSearch}/>
+        <Button label="Minhas consultas" onPress={goToCalendar} />
+        <Button label="Buscar profissionais" onPress={goToSearch} />
       </View>
       <View style={styles.footer}>
-        <Footer/>
+        <Footer />
       </View>
     </Background>
   );
-}
+};
 
-const createStyles = (theme :Theme) => {
+const createStyles = (theme: Theme) => {
   const styles = StyleSheet.create({
     contentPrimary: {
       width: "100%",
       height: "40%",
       justifyContent: "space-between",
       paddingTop: 25,
-      alignItems: "center"
+      alignItems: "center",
     },
-  
+
     buttonCircle: {
       width: 190,
       height: 190,
@@ -170,9 +172,9 @@ const createStyles = (theme :Theme) => {
       justifyContent: "center",
       alignItems: "center",
       marginTop: "10%",
-      zIndex: 10
+      zIndex: 10,
     },
-  
+
     contentBorderButton: {
       width: 175,
       height: 175,
@@ -183,38 +185,34 @@ const createStyles = (theme :Theme) => {
       justifyContent: "center",
       alignItems: "center",
     },
-  
+
     contentSecondary: {
       width: "100%",
       alignItems: "center",
       justifyContent: "center",
       paddingHorizontal: 20,
       height: "30%",
-      
     },
-  
+
     labelButtonCircle: {
       fontSize: 20,
       textAlign: "center",
       color: theme.titleButton,
-      fontFamily: "Inter_600SemiBold"
+      fontFamily: "Inter_600SemiBold",
     },
-  
-  
+
     titleHome: {
       color: theme.textVariant,
       fontSize: 24,
-      fontFamily: "Inter_600SemiBold"
-  
+      fontFamily: "Inter_600SemiBold",
     },
-  
+
     footer: {
       flex: 1,
       justifyContent: "flex-end",
       alignItems: "flex-end",
-  
-    }
-  })
+    },
+  });
   return styles;
 };
 
